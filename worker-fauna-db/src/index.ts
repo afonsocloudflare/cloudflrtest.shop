c397c536239b0bc6aa119b58d2a34e58f17768c7
afonsocloudflare <afonso@cloudflare.com>
2024-05-07 16:38:41 +0100

    update fauna setup

--- worker-fauna-db/src/index.ts
+++ worker-fauna-db/src/index.ts
@@ -1,13 +1,46 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { Client, fql, ServiceError } from 'fauna';
 
 type Bindings = {
-  [key in keyof CloudflareBindings]: CloudflareBindings[key]
-}
+  FAUNA_SECRET: string;
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+type Variables = {
+  faunaClient: Client;
+};
+
+type Product = {
+  id: string;
+  serialNumber: number;
+  title: string;
+  weightLbs: number;
+  quantity: number;
+};
+
+const app = new Hono<{ Bindings: Bindings; Variables: Variables }>();
+
+app.use('*', async (c, next) => {
+  const faunaClient = new Client({
+    secret: c.env.FAUNA_SECRET,
+  });
+  c.set('faunaClient', faunaClient);
+  await next();
+});
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello World');
+});
 
-export default app
+export default app;
+
+app.post('/products', async (c) => {
+  const { serialNumber, title, weightLbs } = await c.req.json<Omit<Product, 'id'>>();
+  const query = fql`Products.create({
+    serialNumber: ${serialNumber},
+    title: ${title},
+    weightLbs: ${weightLbs},
+    quantity: 0
+  })`;
+  const result = await c.var.faunaClient.query<Product>(query);
+  return c.json(result.data);
+});
