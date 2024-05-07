8b0fdb17c0d7d18d353f1ce71af7234d693e109d
afonsocloudflare <afonso@cloudflare.com>
2024-05-07 11:33:29 +0100

    basic html site

--- server/utils/adminsOnly.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  if (req.user && req.user.isAdmin) next()
-  else {
-    const err = new Error('Unauthorized!')
-    err.status = 401
-    next(err)
-  }
-}
--- server/utils/isCurrentUser.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res, next) => {
-  if (Number(req.user.id) === Number(req.params.id)) next()
-  else {
-    const err = new Error('Unauthorized!')
-    err.status = 401
-    next(err)
-  }
-}
