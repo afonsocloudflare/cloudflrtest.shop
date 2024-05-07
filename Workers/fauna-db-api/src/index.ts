f4fb3637016e48bdbf7e2182dade230c419a0b4d
afonsocloudflare <afonso@cloudflare.com>
2024-05-07 17:29:59 +0100

    things

--- Workers/fauna-db-api/src/index.ts
+++ Workers/fauna-db-api/src/index.ts
@@ -1,13 +1,42 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { Client, fql, ServiceError } from 'fauna';
 
 type Bindings = {
-  [key in keyof CloudflareBindings]: CloudflareBindings[key]
-}
+  FAUNA_SECRET: string;
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+type Variables = {
+  faunaClient: Client;
+};
+
+type Product = {
+  id: string;
+  serialNumber: number;
+  title: string;
+  weightLbs: number;
+  quantity: number;
+};
+
+const app = new Hono<{ Bindings: Bindings; Variables: Variables }>();
+
+app.use('*', async (c, next) => {
+  const faunaClient = new Client({
+    secret: c.env.FAUNA_SECRET,
+  });
+  c.set('faunaClient', faunaClient);
+  await next();
+});
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello World');
+});
 
-export default app
+export default app;
+
+app.use('*', async (c, next) => {
+  const faunaClient = new Client({
+    secret: c.env.FAUNA_SECRET,
+  });
+  c.set('faunaClient', faunaClient);
+  await next();
+});
