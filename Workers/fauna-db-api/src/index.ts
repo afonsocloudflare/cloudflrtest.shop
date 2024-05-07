4e88486f06ece50e692df53c539708e2d17979cc
afonsocloudflare <afonso@cloudflare.com>
2024-05-07 18:10:54 +0100

    g

--- Workers/fauna-db-api/src/index.ts
+++ Workers/fauna-db-api/src/index.ts
@@ -33,10 +33,42 @@ app.get('/', (c) => {
 
 export default app;
 
-app.use('*', async (c, next) => {
-  const faunaClient = new Client({
-    secret: c.env.FAUNA_SECRET,
-  });
-  c.set('faunaClient', faunaClient);
-  await next();
+app.post('/products', async (c) => {
+  const { serialNumber, title, weightLbs } = await c.req.json<Omit<Product, 'id'>>();
+  const query = fql`Products.create({
+    serialNumber: ${serialNumber},
+    title: ${title},
+    weightLbs: ${weightLbs},
+    quantity: 0
+  })`;
+  const result = await c.var.faunaClient.query<Product>(query);
+  return c.json(result.data);
+});
+
+app.get('/products/:productId', async (c) => {
+  const productId = c.req.param('productId');
+  const query = fql`Products.byId(${productId})`;
+  const result = await c.var.faunaClient.query<Product>(query);
+  return c.json(result.data);
+});
+
+app.delete('/products/:productId', async (c) => {
+  const productId = c.req.param('productId');
+  const query = fql`Products.byId(${productId})!.delete()`;
+  const result = await c.var.faunaClient.query<Product>(query);
+  return c.json(result.data);
+});
+
+app.onError((e, c) => {
+  if (e instanceof ServiceError) {
+    return c.json(
+      {
+        status: e.httpStatus,
+        code: e.code,
+        message: e.message,
+      },
+    );
+  }
+  console.trace(e);
+  return c.text('Internal Server Error', 500);
 });
